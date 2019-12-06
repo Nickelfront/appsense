650cc7294412850f7fcd073cb8e442285c1cbee7
Nick_HomePC <Nick@DESKTOP-0TIULIO>
2019-12-06 09:17:16 +0200

    added fullcalendar

---
+++ public/dashboard/calendar.php
@@ -0,0 +1,79 @@
+<?php
+include_once "../../app/bootstrap.php";
+
+use Util\Template;
+
+$pageName = "Dashboard | Company details";
+$templateDir = "public/dashboard"; 
+
+// if (!isset($_GET['id'])) {
+//     header("location: index.php");
+// }
+
+init_dashboard($currentUser, Template::header($pageName, $templateDir));
+
+// $company = $db->getCompany($_GET['id']);
+
+?>
+<!-- <script src="assets/scripts/calendar.js"></script> -->
+
+<div class="app-main__outer">
+    <div class="app-main__inner">
+        <div class="app-page-title">
+            <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                    <div class="page-title-icon">
+                        <i class="pe-7s-display2 icon-gradient bg-sunny-morning"></i>
+                    </div>
+                    <div>Company details
+                        <div class="page-title-subheading">View your details available to everyone who can see your
+                            profile.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="main-card mb-3 card">
+                    <div class="card-body">
+                        <div id='absence-calendar'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="./assets/fullcalendar-4.3.1/packages/timegrid/main.min.js"></script>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        var calendarEl = document.getElementById('absence-calendar');
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            plugins: ['dayGrid'],
+            // defaultView: 'timeGridWeek',
+            timeZone: 'local',
+            defaultDate: new Date().getTime(), // will be parsed as local
+
+            events: {
+                url: '../endpoints/company-absences.php',
+                method: 'POST',
+                extraParams: {
+                    user_token: "<?php echo $_SESSION['userData']['login_token']; ?>"
+                },
+                failure: function () {
+                    alert('there was an error while fetching events!');
+                },
+                color: 'blue', // a non-ajax option
+                textColor: 'white' // a non-ajax option
+            }
+        });
+
+        calendar.render();
+    });
+</script>
+
+<?php
+echo Template::footer($templateDir);
